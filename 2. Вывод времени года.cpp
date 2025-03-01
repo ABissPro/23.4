57ba76c2b6fa1667288999fe00ad4fbc15787e7f
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-03-01 21:24:53 +0300

    Add files via upload

---
+++ 2. Вывод времени года.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+//Определите один из этих макросов перед компиляцией!
+ #define SPRING
+// #define SUMMER
+// #define AUTUMN
+// #define WINTER
+
+#if defined(SPRING) && !defined(SUMMER) && !defined(AUTUMN) && !defined(WINTER)
+int main() {
+    std::cout << "Spring" << std::endl;
+}
+#elif defined(SUMMER) && !defined(SPRING) && !defined(AUTUMN) && !defined(WINTER)
+int main() {
+    std::cout << "Summer" << std::endl;
+}
+#elif defined(AUTUMN) && !defined(SPRING) && !defined(SUMMER) && !defined(WINTER)
+int main() {
+    std::cout << "Autumn" << std::endl;
+}
+#elif defined(WINTER) && !defined(SPRING) && !defined(SUMMER) && !defined(AUTUMN)
+int main() {
+    std::cout << "Winter" << std::endl;
+}
+#else
+#error "Ровно один макрос должен быть определен: SPRING, SUMMER, AUTUMN или WINTER"
+#endif
